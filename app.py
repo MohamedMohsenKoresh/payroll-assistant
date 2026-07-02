1457e1aeabf3fe1ad18da2614136c626b966bf9f
MohamedMohsenKoresh <mohamed.mohsen.1983.ps@gmail.com>
2026-07-02 10:39:09 +0300

    Refactor project structure and update ML model

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import sys
+
+print(sys.stdout.encoding)
+print(sys.stdin.encoding)
+
+print("محمد")
+
+
 from data_loader import load_data
+
 from payroll_service import get_employee_data
 from intents import detect_intent
 from payroll_chat_engine import handle_question
@@ -8,9 +17,12 @@ from conversation_manager import save_context
 from conversation_manager import get_last_intent
 from conversation_manager import get_last_topic
 
-from text_normalizer import normalize_text
+from services.text_normalizer import normalize_text
 
 df = load_data()
+
+print("Hello")
+print("ادخل رقم الموظف")
 
 employee_id = int(input("ادخل رقم الموظف: "))
 
